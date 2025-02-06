4de5e680c836480b81f37c29006a87b65c77b4a6
탁제원 <tak@ddock.kr>
2025-02-06 15:59:48 +0900

    [add] ag-grid, ant-design 연동

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,22 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
+import Antd from "ant-design-vue";
+import "ant-design-vue/dist/reset.css";
 
-createApp(App).use(router).mount("#app");
+import {
+  AllCommunityModule,
+  ModuleRegistry,
+  provideGlobalGridOptions,
+} from "ag-grid-community";
+
+// Register all community features
+ModuleRegistry.registerModules([AllCommunityModule]);
+
+// Mark all grids as using legacy themes
+provideGlobalGridOptions({ theme: "legacy" });
+
+const app = createApp(App);
+app.use(Antd); // Use ant-design-vue globally
+app.use(router); // Use the router globally
+app.mount("#app");
