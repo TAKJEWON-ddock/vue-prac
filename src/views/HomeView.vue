9acce304ff0f3df8b0fd374978e3f3f1660a6a6c
탁제원 <tak@ddock.kr>
2025-02-07 11:28:24 +0900

    회원가입 폼 UI 추가 (#1)
    * [add] ag-grid, ant-design 연동
    
    * [add] 회원가입 화면 추가
    [change] 폰트 변경경
    
    * [fix] date picker placeholder 문구 변경경
    
    * [fix] Option API -> Composition API

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,78 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <ag-grid-vue
+      :rowData="rowData"
+      :columnDefs="colDefs"
+      :theme="myTheme"
+      :rowSelection="rowSelection"
+      style="height: 80vh; width: auto"
+    >
+    </ag-grid-vue>
   </div>
 </template>
 
 <script lang="ts">
-import { Options, Vue } from "vue-class-component";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+import { ref } from "vue";
+import { themeQuartz, RowSelectionOptions } from "ag-grid-community";
+import { AgGridVue } from "ag-grid-vue3"; // Vue Data Grid Component
 
-@Options({
+export default {
+  name: "HomeView",
   components: {
-    HelloWorld,
+    AgGridVue,
   },
-})
-export default class HomeView extends Vue {}
+  setup() {
+    const myTheme = themeQuartz.withParams({
+      headerHeight: 35,
+      rowHeight: 35,
+      columnBorder: { style: "dotted", color: "#9696C8" },
+    });
+
+    const rowSelection = ref<RowSelectionOptions | "single" | "multiple">({
+      mode: "multiRow",
+    });
+    // Row Data: The data to be displayed.
+    const rowData = ref([
+      { make: "Tesla", model: "Model Y", price: 64950, electric: true },
+      { make: "Ford", model: "F-Series", price: 33850, electric: false },
+      { make: "Toyota", model: "Corolla", price: 29600, electric: false },
+    ]);
+
+    // Column Definitions: Defines the columns to be displayed.
+    const colDefs = ref([
+      {
+        field: "make",
+        cellRenderer: (params: any) => {
+          // put the value in bold
+          return "Value is <b>" + params.value + "</b>";
+        },
+        filter: true,
+        floatingFilter: true,
+      },
+      {
+        field: "model",
+        filter: true,
+        floatingFilter: true,
+      },
+      {
+        field: "price",
+        valueFormatter: (p: any) => "£" + p.value.toLocaleString(),
+        filter: true,
+        floatingFilter: true,
+      },
+      {
+        field: "electric",
+        filter: true,
+        floatingFilter: true,
+      },
+    ]);
+
+    return {
+      myTheme,
+      rowSelection,
+      rowData,
+      colDefs,
+    };
+  },
+};
 </script>
